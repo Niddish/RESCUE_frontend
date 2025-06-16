41dfe26e1b35b47d9f3cf2fd6445ab4910cbb54c
nwarre <121927267+Niddish@users.noreply.github.com>
2025-06-16 12:13:52 -0400

    Update DataParallelBar.jsx

--- src/components/DataParallelBar.jsx
+++ src/components/DataParallelBar.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Node from "./Node_4_GPU";
+import "../css/DataParallelBar.css"; 
 
 export default function DataParallelBar({ nodesForRow, gpuList, selectedParallelisms, showPlaceholderGPUs }) {
   const [hoveredGPU, setHoveredGPU] = React.useState(null);
@@ -22,31 +23,7 @@ export default function DataParallelBar({ nodesForRow, gpuList, selectedParallel
   if (filteredNodesForRow.length === 0) return null;
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        width: "100%",
-        maxWidth: "100%",
-        height: "auto",
-        maxHeight: "150px",
-        backgroundColor: "#5C5555",
-        overflowX: "scroll",
-        overflowY: "hidden",
-        whiteSpace: "nowrap",
-        gap: "10px",
-        marginBottom: "10px",
-        "&::-webkit-scrollbar": { height: "8px" },
-        "&::-webkit-scrollbar-track": { background: "#5C5555", borderRadius: "10px" },
-        "&::-webkit-scrollbar-thumb": {
-          background: "linear-gradient(180deg,rgb(160, 152, 152),rgb(90, 100, 100))",
-          borderRadius: "10px"
-        },
-        "&::-webkit-scrollbar-thumb:hover": {
-          background: "linear-gradient(180deg,rgb(160, 152, 152),rgb(90, 100, 100))"
-        }
-      }}
-    >
+    <Box className="scrollContainer">
       {filteredNodesForRow.map(([nodeName, gpus], index) => (
         <Node
           key={index}
